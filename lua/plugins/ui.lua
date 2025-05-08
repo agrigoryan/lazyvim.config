fe833aebe2acd5d3b09c172eb066da225b93ae56
agrigoryan <aram.grigoryan@gmail.com>
2025-05-08 18:32:48 -0400

    by default show ignored files and show hidden files in the explorer as well

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,6 +4,15 @@ return {
     opts = {
       scroll = { enabled = false },
       indent = { enabled = false },
+      picker = {
+        sources = {
+          files = { ignored = true },
+          explorer = { ignored = true, hidden = true },
+          grep = { ignored = true },
+          grep_word = { ignored = true },
+          grep_buffers = { ignored = true },
+        },
+      },
     },
   },
 }
