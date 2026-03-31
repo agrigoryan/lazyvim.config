9da98862136250394444c4f6e737b9a8796a6444
agrigoryan <aram.grigoryan@gmail.com>
2026-03-30 23:28:50 -0400

    update neovim dependencies

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,23 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Re-sign treesitter parsers on macOS to fix codesigning crashes (com.apple.provenance xattr)
+if vim.fn.has("mac") == 1 then
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = vim.api.nvim_create_augroup("ts_codesign", { clear = true }),
+    callback = function()
+      local parser_dir = vim.fn.stdpath("data") .. "/site/parser"
+      vim.system({
+        "bash",
+        "-c",
+        string.format(
+          'if xattr -l "%s"/*.so 2>/dev/null | grep -q provenance; then xattr -cr "%s"/*.so && codesign -fs - "%s"/*.so; fi',
+          parser_dir,
+          parser_dir,
+          parser_dir
+        ),
+      })
+    end,
+  })
+end
